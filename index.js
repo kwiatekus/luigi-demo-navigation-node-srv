ad5f780ef39b5f398775ba9065bcdd80fb107f90
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2018-09-24 12:33:14 +0200

    cors

--- index.js
+++ index.js
@@ -11,27 +11,20 @@ app.get('/', (request, response) => response.send('OK'));
 // all routes prefixed with /api
 app.use('/api', router);
 
-
   // this array is used for identification of allowed origins in CORS
   const originWhitelist = ['http://localhost:4200'];
 
-  // middleware route that all requests pass through
+  // middleware CORS handling route that all requests pass through
   router.use((request, response, next) => {
-    console.log('Server info: Request received');
-    
     let origin = request.headers.origin;
     
-    // only allow requests from origins that we trust
     if (originWhitelist.indexOf(origin) > -1) {
       response.setHeader('Access-Control-Allow-Origin', origin);
     }
     
-    // only allow get requests, separate methods by comma e.g. 'GET, POST'
     response.setHeader('Access-Control-Allow-Methods', 'GET');
     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     response.setHeader('Access-Control-Allow-Credentials', true);
-    
-    // push through to the proper route
     next();
   });
 
