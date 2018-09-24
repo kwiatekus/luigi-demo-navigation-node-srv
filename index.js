8d4507b8b0fd16931b0ded2aa2d3dd84bbdabfda
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2018-09-24 10:44:14 +0200

    return dummy navigation node data

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ const router = express.Router();
 const port = 8080;
 const url = require('url');
 
+
 // url: http://localhost:8080/
 app.get('/', (request, response) => response.send('OK'));
 
 // all routes prefixed with /api
 app.use('/api', router);
 
-router.get('/stuff', (request, response) => {
+router.get('/navigation', (request, response) => {
     var urlParts = url.parse(request.url, true);
     var parameters = urlParts.query;
-    var myParam = parameters.myParam;
-    // e.g. myVenues = 12;
+    var email = parameters.email;
     
-    var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
+    var nodes = [{
+        label: 'Catalog Manager',
+        pathSegment: 'catmanager',
+        viewUrl: '/assets/dummy.html'
+    },{
+        label: 'Product Variants',
+        pathSegment: 'variants',
+        viewUrl: '/assets/dummy.html'
+    },{
+        label: 'Units',
+        pathSegment: 'units',
+        viewUrl: '/assets/dummy.html'
+    },{
+        label: 'Administration',
+        pathSegment: 'administration',
+        viewUrl: '/assets/dummy.html'
+    }];
     
-    response.json({message: myResponse});
+    response.json(nodes);
   });
 
 // set the server to listen on port 8080
